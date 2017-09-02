445333846214e57a5336539bfd47d8ff3c12f0a9
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2017-09-02 12:26:58 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+if(a>b) && (a>c)
+{
+printf("a is big");
+}
+else if(b>c)
+{
+printf("b is big");
+}
+else
+{
+printf("c is big");
+}
+getch();
+}
