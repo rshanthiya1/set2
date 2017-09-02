dbf24c77ed463c49ecf9c45e7d55acd705dce9ba
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2017-09-02 12:38:07 +0530

    Create hai.c

---
+++ hai.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x;
+printf("enter the year");
+scanf("%d",&x);
+if(x%4==0)
+{
+printf("lipear");
+}
+else
+{
+printf("not lipear");
+}
+getch();
+}
