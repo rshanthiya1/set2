d69223abe21e74cb4ef93da32e527cebdd9ee854
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2017-09-04 06:27:54 +0530

    Create ASCII.c

---
+++ ASCII.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main()
+{
+  char c;
+  
+  printf("\n Please Enter any character \n");
+  scanf("%c",&c);
+  
+  printf("\n The ASCII value of given character = %d",c);
+  return 0;
+}
