5e44e9bdfc2b99428b56110799205a10e0702a10
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2017-09-05 04:07:54 -0700

    Create q.c

---
+++ q.c
@@ -0,0 +1,21 @@
+
+int char ch;
+int i;
+
+clrscr();
+void main(){
+
+printf("Print 1 to 5 again and again");
+while(1){
+for(i=1;i<=5;i++)
+
+     printf("\n%d",i);
+
+    ch=getch();
+    if(ch=='Q')
+     exit(0);
+
+    }
+
+    getch();
+    }
