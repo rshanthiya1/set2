f070e18f2b86c4ca14950d49966e3238e9695de9
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2017-09-08 16:35:31 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100 
+
+
+int main()
+{
+    char string[MAX_SIZE];
+    int alphabets, digits, others, i;
+
+    alphabets = digits = others = i = 0;
+    printf("\nEnter any string : ");
+    gets(string);
+    while(string[i]!='\0')
+    {
+        if((string[i]>='a' && string[i]<='z') || (string[i]>='A' && string[i]<='Z'))
+        {
+            alphabets++;
+        }
+        else if(string[i]>='0' && string[i]<='9')
+        {
+            digits++;
+        }
+        else
+        {
+            others++;
+        }
+
+        i++;
+    }
+
+    printf("\nAlphabets = [ %d ]\n", alphabets);
+    printf("\nDigits = [ %d ]\n", digits);
+    printf("\nSpecial characters = [ %d ]\n", others);
+
+    return 0;
+}
