5d4d66fe521cedda069a0f826b35b74bb827e040
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-01-08 11:19:07 +0000

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i,a;
+printf("enter the value");
+scanf("%d",&a);
+for(i=1;i<=10;i++)
+{
+printf("%d",i);
+}
+if(a==i);
+{
+printf("yes");
+else
+{
+printf("no");
+}
+getch();
+}
