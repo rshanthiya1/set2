fdfbb2dd05ada035c34b8421f60f958d8403e877
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-01-08 10:38:27 +0000

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,i;
+printf("enter the value");
+scanf("%d",&a);
+for(i=1;i<a;i++)
+{
+printf("%d",i\n);
+}
+if(a/2!=0)
+{
+printf("the average value is:");
+}
+getch();
+}
