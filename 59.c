8231265c8124a464fc86b343ab9507768a725d90
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-01-21 13:32:54 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a[i],i,b=0;
+printf("enter the values");
+scanf("%d",&a[i]);
+for(i=0;i<=a[i];i++)
+{
+b=a[i];
+if(a[i]>b)
+{
+b=a[i];
+printf("%d",b);
+}
+}
+getch();
+}
