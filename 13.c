653d54960c1fc3a09cbe234849f654630f2c4c7f
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-02-02 14:47:09 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int x,i,c=0;
+i=2;
+printf("enter the value");
+scanf("%d",&x);
+while(i<x)
+{
+if(x%i==0)
+{
+c=c+1;
+}
+i++;
+}
+if(c==0)
+{
+printf("prime");
+}
+else
+{
+printf("not a prime number");
+}
+}
