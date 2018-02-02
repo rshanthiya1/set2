b2ed53f8f7bae5a905c26cf05f8a16424bd893d9
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-02-02 14:20:39 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int x,s,a=0,y;
+printf("enter the value:");
+Scanf(" %d",&x);
+y=x;
+while(x>0)
+{
+s=(x%10);
+a=a+(s*s*s);
+x=x/10;
+}
+if(y==a)
+{
+printf("Armstrong ");
+}
+else
+{
+printf(" not  Armstrong ");
+}
+}
