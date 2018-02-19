42f48885936c453e64604e45e2ea979b1e4d963b
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-02-19 16:16:00 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int x,s=0;
+    printf("enter the value");
+    scanf("%d",&x);
+    if(x>0 && x<9)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
+
+
