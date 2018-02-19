a6a8567fe485d472ba022e158d1ffd63c2fcc164
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-02-19 16:41:05 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,i,s=0,a;
+    printf("enter the value");
+    scanf("%d",&x);
+    for(i=0;i<x;i++)
+    {
+      printf("%d",i);
+    }
+      s=x/2;
+          printf("\n the medium element is:%d",s);
+          
+}
+   
