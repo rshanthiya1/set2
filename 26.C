fefb2ea22bcb96cb6a48537c2e6e6fa5836262ed
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-02-20 06:50:33 +0530

    Create 26.C

---
+++ 26.C
@@ -0,0 +1,32 @@
+
+include <stdio.h>
+int main()
+{
+    int i,j,t,a[10],s;
+    printf("enter the value");
+    scanf("%d",&s);
+    printf("enter the %d value",s);
+    for(i=0;i<s;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<s;i++)
+    {
+        for(j=i+1;j<s;j++)
+        {
+            if(a[i]>a[j])
+            {
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+            }
+        }
+    }
+    printf("the asending order is:");
+    for(i=0;i<s;i++)
+    {
+        printf("%d",a[i]);
+    }
+}
+
+
