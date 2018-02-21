4fb300978d19cc8fa52f5da6fd2df877bdde54f9
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-02-21 16:47:28 +0530

    Create 38 8.c

---
+++ 38 8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i = 65;
+    int k = 120;
+    printf(" value of i=%d k=%d before swapping", i, k);
+
+    i = i ^ k;
+    k = i ^ k;
+    i = i ^ k;
+    printf("value of i=%d k=%d after swapping", i, k);
+
+    return 0;
+}
