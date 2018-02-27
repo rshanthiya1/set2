232e403ecd103b7c758290c22aa46ed1b77a0fa5
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-02-27 19:35:38 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,12 @@
+#inlcude<iostream.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  clrscr();
+  cout<<"enter time in minutes";
+  cin>>a;
+  b=a/60;
+  c=a%60;
+  cout<<b<<"hours"<<c<<"minutes";
+getch();
