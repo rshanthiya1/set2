a5e639a0f1d869bd8c7f74a0ebfb510186b68f6e
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-03-01 05:46:34 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,s,c=0;
+    printf("enter the two value");
+    scanf("%d%d",&n,&s);
+    c=n-s;
+    if(c%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    
+}
+
+
+
