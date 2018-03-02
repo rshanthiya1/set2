4bad91f3ffa7d0f1cca7b894172b81d7d9f0affe
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-03-02 16:52:52 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the value");
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+
+}
+
