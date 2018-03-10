efe8aceff8b3659ef9a77d97748b88fc7996f79a
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-03-10 20:11:21 +0530

    Create 84.c

---
+++ 84.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    int s;
+    printf("enter the charecter:");
+    scanf("%c",&a);
+    s=a;
+    printf("the ascii value of given charecter is:%d",s);
+}
