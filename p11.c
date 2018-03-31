45f7bc0d815c8b8c46706c0e614cd247f8335413
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2018-03-31 21:15:44 +0530

    Create p11.c

---
+++ p11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[56];
+    printf("enter the string:");
+    scanf("%s",&s);
+    if(s[0]=='s')
+    {
+        printf("yes");
+    }
+    
+        else
+        {
+            printf("no");
+        }
+}
